fab6db49e8fcd2a0852295f71d8b0a6fc04b568c
cloud9cloud9 <nalezhitiyvlad@gmail.com>
2024-08-17 13:47:25 +0300

    add some another test

--- test/queue_test.go
+++ test/queue_test.go
@@ -1,10 +1,50 @@
 package test
 
 import (
+	. "github.com/smartystreets/goconvey/convey"
 	"github.com/stretchr/testify/require"
 	q "queue/queue"
 	"testing"
 )
+
+func TestQueue(t *testing.T) {
+
+	Convey("TestQueue", t, func() {
+		queue := q.New[int]()
+		queue.Enqueue(1)
+		queue.Enqueue(2)
+
+		Convey("TestQueueEnqueue", func() {
+
+			elementHead, _ := queue.Peek()
+			So(elementHead, ShouldEqual, 1)
+			queue.Enqueue(3)
+			So(elementHead, ShouldEqual, 1)
+		})
+
+		Convey("TestQueueDequeue", func() {
+
+			elementHead, _ := queue.Peek()
+			So(elementHead, ShouldEqual, 1)
+			deletedValue, err := queue.Dequeue()
+			if err != nil {
+				t.Error(err)
+			}
+			So(deletedValue, ShouldEqual, 1)
+			So(err, ShouldBeNil)
+			elementHead, _ = queue.Peek()
+			So(elementHead, ShouldEqual, 2)
+		})
+
+		Convey("TestQueuePeek", func() {
+			value, err := queue.Peek()
+			if err != nil {
+				t.Error(err)
+			}
+			So(value, ShouldEqual, 1)
+		})
+	})
+}
 
 func TestQueuePeek(t *testing.T) {
 
